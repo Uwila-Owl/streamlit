ff064ed0b2817db13b8f3a88dd6d3887bae2c1b4
Léa MOULINNEUF <lea.moulinneuf@gmail.com>
2024-07-28 12:13:54 +0200

    Modification du mode de fonctionnement

--- streamlit_4.2.py
+++ streamlit_4.2.py
@@ -8,19 +8,63 @@
 #********************************************************/
 
 import streamlit as st
-import pandas as pd
 from PIL import Image
+import piexif
 
-# Charger l'image (remplacez 'votre_image.jpg' par le chemin de votre image)
-image_path = 'photo_lea.jpg'
-image = Image.open(image_path)
+def main():
+    st.title("Modification des métadonnées EXIF")
 
-# Extraire les métadonnées EXIF (par exemple, la date de création, l'appareil photo, etc.)
-exif_data = image._getexif()
+    # Téléchargement de l'image
+    uploaded_image = st.file_uploader("Téléchargez votre image", type=["jpg", "png"])
 
-# Créer un dataframe pandas avec les métadonnées
-df = pd.DataFrame(exif_data.items(), columns=['Attribut', 'Valeur'])
+    if uploaded_image:
+        image = Image.open(uploaded_image)
 
-# Afficher le widget st.data_editor
-st.data_editor(df)
+        # Affichage des métadonnées EXIF
+        st.subheader("Métadonnées EXIF actuelles :")
+        exif_dict = piexif.load(image.info["exif"])
+        for ifd in ("0th", "Exif", "GPS", "1st"):
+            for tag in exif_dict[ifd]:
+                tag_name = piexif.TAGS[ifd][tag]["name"]
+                value = exif_dict[ifd][tag]
+                if isinstance(value, bytes):
+                    value = value.decode("utf-8", errors="ignore")
+                elif isinstance(value, tuple):
+                    value = "".join(chr(i) for i in value if i != 0)
+                st.write(f"{tag_name}: {value}")
 
+        # Modification des métadonnées
+        st.subheader("Modifier les métadonnées EXIF :")
+        new_make = st.text_input("Nouvelle valeur pour 'Make'", exif_dict["0th"][271])
+        new_model = st.text_input("Nouvelle valeur pour 'Model'", exif_dict["0th"][272])
+        new_artist = st.text_input("Nouvelle valeur pour 'Artist'", exif_dict["0th"][315])
+        new_copyright = st.text_input("Nouvelle valeur pour 'Copyright'", exif_dict["0th"][33432])
+        new_xp_author = st.text_input("Nouvelle valeur pour 'XPAuthor'", "".join(chr(i) for i in exif_dict["0th"][40093] if i != 0))
+
+        # Mettre à jour les métadonnées
+        exif_dict["0th"][271] = new_make
+        exif_dict["0th"][272] = new_model
+        exif_dict["0th"][315] = new_artist
+        exif_dict["0th"][33432] = new_copyright
+        exif_dict["0th"][40093] = tuple(ord(c) for c in new_xp_author) + (0,)
+
+        # Affichage des nouvelles valeurs
+        st.subheader("Nouvelles valeurs des métadonnées :")
+        for ifd in ("0th", "Exif", "GPS", "1st"):
+            for tag in exif_dict[ifd]:
+                tag_name = piexif.TAGS[ifd][tag]["name"]
+                value = exif_dict[ifd][tag]
+                if isinstance(value, bytes):
+                    value = value.decode("utf-8", errors="ignore")
+                elif isinstance(value, tuple):
+                    value = "".join(chr(i) for i in value if i != 0)
+                st.write(f"{tag_name}: {value}")
+
+        # Bouton d'enregistrement
+        if st.button("Enregistrer l'image"):
+            exif_bytes = piexif.dump(exif_dict)
+            image.save("image_modifiee.jpg", "jpeg", exif=exif_bytes)
+            st.success("Image enregistrée avec succès !")
+
+if __name__ == "__main__":
+    main()
