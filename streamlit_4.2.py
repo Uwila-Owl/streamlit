0e0c1099d053e1deb0b8e9f55ecb5f60a32c6ebb
Léa MOULINNEUF <lea.moulinneuf@gmail.com>
2024-07-29 12:52:44 +0200

    Version final du programme

--- streamlit_4.2.py
+++ streamlit_4.2.py
@@ -10,6 +10,13 @@
 import streamlit as st
 from PIL import Image
 import piexif
+import pandas as pd
+from streamlit_folium import st_folium
+import folium
+
+#
+# Modification des métadonnées d'une image 
+#
 
 def main():
     st.title("Modification des métadonnées EXIF")
@@ -60,6 +67,36 @@ def main():
                     value = "".join(chr(i) for i in value if i != 0)
                 st.write(f"{tag_name}: {value}")
 
+        # Modification des données GPS
+        st.subheader("Modifier les données GPS :")
+        new_gps_latitude = st.text_input("Nouvelle latitude", "43.296482")
+        new_gps_longitude = st.text_input("Nouvelle longitude", "5.36978")
+        new_gps_altitude = st.text_input("Nouvelle altitude", "0")
+
+        # Mise à jour des données GPS
+        if new_gps_latitude and new_gps_longitude and new_gps_altitude:
+            latitude = float(new_gps_latitude)
+            longitude = float(new_gps_longitude)
+            altitude = int(new_gps_altitude)
+    
+            latitude_degres = int(latitude)
+            latitude_minutes = int((latitude - latitude_degres) * 60)
+            latitude_secondes = int(((latitude - latitude_degres) * 3600) % 60)
+    
+            longitude_degres = int(longitude)
+            longitude_minutes = int((longitude - longitude_degres) * 60)
+            longitude_secondes = int(((longitude - longitude_degres) * 3600) % 60)
+    
+            exif_dict["GPS"][piexif.GPSIFD.GPSLatitude] = ((latitude_degres, 1), (latitude_minutes, 1), (latitude_secondes, 1))
+            exif_dict["GPS"][piexif.GPSIFD.GPSLongitude] = ((longitude_degres, 1), (longitude_minutes, 1), (longitude_secondes, 1))
+            exif_dict["GPS"][piexif.GPSIFD.GPSAltitude] = (altitude, 1)
+        else:
+            st.error("Veuillez remplir tous les champs de données GPS")
+
+        # Affichage des coordonnées GPS sur une carte
+        map_data = pd.DataFrame({'lat': [latitude], 'lon': [longitude]})
+        st.map(map_data)
+
         # Bouton d'enregistrement de l'image
         if st.button("Enregistrer l'image"):
             exif_bytes = piexif.dump(exif_dict)
@@ -68,3 +105,57 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+        
+#
+# Carte sur les pays visités et à visiter 
+#
+# Les lieux que j'ai visités 
+def map_visited_places():
+    st.title("Carte des pays visités")
+
+    # Création de la carte
+    m = folium.Map(location=[45.5236, -122.6750], zoom_start=4)
+
+    # Ajout des POI pour les lieux visités
+    places = ["Heraklion", "Punta Cana", "Rome", "Londres"]
+    latitudes = [35.34336, 18.582010, 41.891930, 54.633221] 
+    longitudes = [25.13608, -68.405473, 12.511330, -3.32277]
+    points = list(zip(latitudes, longitudes))
+    folium.PolyLine(points, color='green').add_to(m)
+    for i, place in enumerate(places):
+        folium.Marker(location=[latitudes[i], longitudes[i]], popup=place).add_to(m)
+
+    # Affichage de la carte
+    st_folium(m, width=800, height=600, key="visited_map")
+
+# Les lieux que je désire visiter
+def map_wanted_places():
+    st.title("Carte des pays à visiter")
+
+    # Création de la carte
+    m = folium.Map(location=[45.5236, -122.6750], zoom_start=4)
+
+    # Ajout des POI pour les lieux à visiter
+    places = ["Tokyo", "Séoul", "Athènes", "Wellington"]
+    latitudes = [34.886306, 37.566535, 38.275283, -40.900557]  
+    longitudes = [134.39711, 126.9779692, 23.810343, 174.885971]  
+    points = list(zip(latitudes, longitudes))
+    folium.PolyLine(points, color='red').add_to(m)
+    for i, place in enumerate(places):
+        folium.Marker(location=[latitudes[i], longitudes[i]], popup=place).add_to(m)
+
+    # Affichage de la carte
+    st_folium(m, width=800, height=600, key="wanted_map")
+
+st.title("Carte sur les pays visités et à visiter")
+st.write("")
+
+# Gestion des cartes par un système d'onglet
+tab1, tab2 = st.tabs(["Pays visités", "Pays à visiter"])
+
+with tab1:
+    map_visited_places()
+
+with tab2:
+    map_wanted_places()
